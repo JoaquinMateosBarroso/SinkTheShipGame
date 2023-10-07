4a2d8bc2e70120aea7ffcf1186a905248918c978
Joaquín Mateos Barroso <jmateosbarroso@gmail.com>
2023-10-07 19:06:46 +0200

    minor

--- SinkTheShip/sinkTheShip.cpp
+++ SinkTheShip/sinkTheShip.cpp
@@ -110,4 +110,6 @@ int main2()
     SinkTheShipClient game;
     game.start("A,A,A,B,B,A,A,B,B,B;A,A,A,A,A,A,A,A,A,A;B,B,A,B,B,A,A,B,B,B;A,A,A,A,A,A,A,A,A,A;A,B,A,B,A,B,A,B,A,A;A,A,A,A,A,A,A,A,A,A;A,A,B,B,B,B,A,A,A,A;A,A,A,A,A,A,A,A,A,A;A,A,A,A,A,A,A,A,A,A;A,A,A,A,A,A,A,A,A,A", 10);
     game.showBoard();
+
+    return 0;
 }
